f04aa1bde77618d91199db4d1a8340921c7a3a13
Raúl <raulivars1997@gmail.com>
2022-10-31 18:54:56 +0100

    sin terminar

--- DWEC_05/activity_8.js
+++ DWEC_05/activity_8.js
@@ -1,26 +1,42 @@
+//declaramos las variables
 const palabras = [];
 let palabra;
-let letras;
+
+//bucle que irá pidiendo palabras hasta que se deje vacio
 do{
     palabra = (prompt("Introduce palabras y te dire las que son anagrams  (dejelo vacio para finalizar)"));
-    palabras.push(palabra)
-    letras = palabra.split("").sort().join("");
+    palabras.push(palabra);
 }   
-    while(palabra !=null && palabra!="");
-
+while(palabra !=null && palabra!="");
 
 function Anagrama(array){
-   
+   const arrOrd = [];
+   let letras;
     for(palabra of array){
-        if(!mapa.has(palabra)){
-            mapa.set(palabra,1);
-        }
-        else{
-            mapa.set(palabra,mapa.get(palabra)+1);
-        }
+        letras = palabra.split("").sort().join("");
+        arrOrd.push(letras);
     }
-    document.write("<hr>")
-    for(let [palabra,numero] of mapa){
-        document.write("<b>Palabra:</b> "+palabra+" ---  <b>Repetida:</b> "+numero+" veces<br><hr>");
+
+    //eliminara el ultimo elemento que se queda vacio al terminar el bucle del prompt.
+    arrOrd.pop();
+    console.log(arrOrd);
+
+
+
+    let cont = 0;
+    for (let i = 1; i <= arrOrd.length; i++) {
+        if(arrOrd[i-1]===arrOrd[i]){
+            cont++;
+        }
+     }
+     
+    if(cont==arrOrd.length+1){
+        alert("Las palabras introducidas son anagramas");
+        return true;
+    }
+    else{
+        alert("Las palabras introducidas no son anagramas");
+        return false;
     }
 }
+Anagrama(palabras);
