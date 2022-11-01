f254f4d89f4ef5e5405da8d580e7b9f674d6be5a
Raúl <raulivars1997@gmail.com>
2022-11-01 12:40:49 +0100

    terminado

--- DWEC_05/activity_6.js
+++ DWEC_05/activity_6.js
@@ -1,40 +1,45 @@
 var valido = false;
 //bucle que pedirá un número y se repetirá en caso de no ser un número o dejarlo vacio
 do{
-    var num = parseInt(prompt("Introduce un numero y te dire su valor de Fibonacci"));  
-    
+    var num = prompt("Introduce un numero y te dire su valor de Fibonacci");  
+    if(num==undefined){
+        valido==true;
+    }
     if(isNaN(num)){
         num = parseInt(prompt("Introduce un numero y te dire su valor de Fibonacci"));
+        if(num==undefined){
+            valido==true;
+        }
     }
     else{
         valido = true;
     }
-}
-while(valido==false || num !=undefined) 
+}while(valido==false) 
 
-/*-----SOLUCION RECURSIVA-----*/
-//creammos la funcion recursiva a la que le pasaremos el valor
-function FibonacciRe(n){
-    if(n!=undefined){
-        if(n<=1){
-            //si el valor es menor o 1 devolvera 1
-            return 1;
-        }
-        else{//si el valor es mayoe que 1 se le sumara a este el resultado de la funcion recursiva de un numero menor que este
-            return n + FibonacciRe(n-1);
-        }   
+//continuara con el programa si no se cancela
+if(num!=undefined){
+    
+    //parseamos el numero dado
+    num = parseInt(num);
+
+    /*-----SOLUCION RECURSIVA-----*/
+    //creammos la funcion recursiva a la que le pasaremos el valor
+    function FibonacciRe(n){
+            if(n<=1){
+                //si el valor es menor o 1 devolvera 1
+                return 1;
+            }
+            else{//si el valor es mayoe que 1 se le sumara a este el resultado de la funcion recursiva de un numero menor que este
+                return n + FibonacciRe(n-1);
+            }   
     }
-    else{
-            alert("Ha cancelado el programa");  
-    }
-}
-//imprimira el resultado de la funcion recursiva
-document.write("Resultado del valor de Fibonacci de "+num+" de forma recursiva: "+FibonacciRe(num)+"<hr>");
+    //imprimira el resultado de la funcion recursiva
+    document.write("Resultado del valor de Fibonacci de "+num+" de forma recursiva: "+FibonacciRe(num)+"<hr>");
 
 
-/*-----SOLUCION NO RECURSIVA-----*/
-function FibonacciNoRe(n){
-    if(n!=undefined){
+    /*-----SOLUCION NO RECURSIVA-----*/
+    function FibonacciNoRe(n){
+   
         let res=0;
         /*bucle que hasta que el valor dado no sea 0 irá guardando en una variable nueva(res) el resultado de la de la resta de 
         esta nueva vaiable menos el valor dado y posteriormente se le restara 1 al valor dado para que termine el bucle al llegar 0 */
@@ -45,8 +50,8 @@ function FibonacciNoRe(n){
         //al hacer el bucle restando el resultado dara negativo, por lo que devolveremos el valor absoluto.
         return Math.abs(res);
     }
-    else{
-        alert("Ha cancelado el programa");  
+    document.write("Resultado del valor de Fibonacci de "+num+" de forma no recursiva: "+FibonacciNoRe(num));
 }
+else{
+    alert("Ha cancelado el programa!");
 }
-document.write("Resultado del valor de Fibonacci de "+num+" de forma no recursiva: "+FibonacciNoRe(num));
