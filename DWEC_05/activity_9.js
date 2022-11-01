83cdf84b8066195a53d06b796aebf472fd51c7fb
Raúl <raulivars1997@gmail.com>
2022-11-01 19:03:46 +0100

    terminado

--- DWEC_05/activity_9.js
+++ DWEC_05/activity_9.js
@@ -42,9 +42,28 @@ if(num!=undefined){
     /*-----SOLUCION NO RECURSIVA-----*/
     function TribonacciNoRe(n){
    
-        
+        //si el valor es menor que 0 devolvera 0
+        if(n==0){
+            return 0;
+        }//si el valor es 1 o 2 devolvera 1
+        if(n==1 || n==2){
+            return 1;
+        }
+        //declaramos los dos numeros anteriores que son los que se iran sumando y el resultado
+        let menos1 = 1;
+        let menos2 = 1;
+        let res = 0;
+        //bucle que ira sumando los tres numeros anteriores hasta llegar el valor dado.
+        for (let i = 0; i<n; i++) {
+            var temp = menos2
+            menos2 =  res + menos1 + menos2;
+            res = menos1;
+            menos1 = temp;
+        }//devolvera el resultado
+        return res;
+    }    
     document.write("Resultado del valor de Tribonacci de "+num+" de forma no recursiva: "+TribonacciNoRe(num)); 
-    }
+    
 }
 else {
     alert("Ha cancelado el programa!");
