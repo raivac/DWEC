92b3040c7271f3c6eabc4a3c3a222befe292acb1
Raúl <raulivars1997@gmail.com>
2023-02-03 17:10:28 +0100

    DWEC

--- DWEC_10/Activity6/js/acciones.js
+++ DWEC_10/Activity6/js/acciones.js
@@ -9,7 +9,7 @@ fetch("https://randomuser.me/api/?results=1000").then(response => {
         fotos[i] = url;
     }
     for (let i = 0; i < 50; i++) {
-        let numRand= parseInt(Math.random() * (fotos.length));
+        let numRand = parseInt(Math.random() * fotos.length);
         let img = document.createElement("img");
         img.setAttribute("src", fotos[numRand]);
         div.appendChild(img);
@@ -17,7 +17,7 @@ fetch("https://randomuser.me/api/?results=1000").then(response => {
     let cont = 0;
     let temp = setInterval(() => {
         if (cont <= 100) {
-            for(let i = 1; i<=50 ; i++){
+            for (let i = 1; i <= 50; i++) {
                 urlNuevo(i)
             }
             cont++;
@@ -28,9 +28,9 @@ fetch("https://randomuser.me/api/?results=1000").then(response => {
 
 })
 function urlNuevo(pos) {
-    let img = document.getElementsByTagName("img")[pos-1];
+    let img = document.getElementsByTagName("img")[pos - 1];
     let nuevaImg = document.createElement("img");
-    let numRand = parseInt(Math.random() * (fotos.length));
+    let numRand = parseInt(Math.random() * fotos.length);
     nuevaImg.src = fotos[numRand];
     div.replaceChild(nuevaImg, img);
 }      
