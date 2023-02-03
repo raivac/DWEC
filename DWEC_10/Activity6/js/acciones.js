020fc49b157d96c72518f31df1e00612b0f9e350
Raúl <raulivars1997@gmail.com>
2023-02-03 14:22:35 +0100

    DWEC

---
+++ DWEC_10/Activity6/js/acciones.js
@@ -0,0 +1,40 @@
+var fotos = [];
+
+var div = document.getElementById("div");
+fetch("https://randomuser.me/api/?results=1000").then(response => {
+    return response.json();
+}).then(result => {
+    for (let i = 0; i < 1000; i++) {
+        let url = result.results[i].picture.large;
+        fotos[i] = url;
+    }
+    for (let i = 0; i < 5; i++) {
+        let numRand= parseInt(Math.random() * (fotos.length));
+        let img = document.createElement("img");
+        img.setAttribute("src", fotos[numRand]);
+        div.appendChild(img);
+        fotos.pop(numRand);
+    }
+    let cont = 0;
+    let temp = setInterval(() => {
+        if (cont <= 100) {
+            for(let i = 1; i<=5 ; i++){
+                urlNuevo(i)
+            }
+            cont++;
+        } else {
+            clearInterval(temp);
+        }
+    }, 200);
+
+})
+
+
+function urlNuevo(pos) {
+    let img = document.getElementsByTagName("img")[pos-1];
+    let nuevaImg = document.createElement("img");
+    let numRand = parseInt(Math.random() * (fotos.length));
+    nuevaImg.src = fotos[numRand];
+    fotos.pop(numRand);
+    div.replaceChild(nuevaImg, img);
+}      
